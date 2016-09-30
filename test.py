de2a074514b6eb35fd6b811ae589a99f6603809e
Peilin Duan <duan@g.harvard.edu>
2016-09-29 21:07:26 -0400

    add test cases to cover all lines

--- test.py
+++ test.py
@@ -2,13 +2,10 @@ import unittest
 from binsearch import *
 import numpy as np
 
+
 class TestBinSearch(unittest.TestCase):
 
 	def test_nan(self):
-		# with self.assertRaises(ValueError):
-		# 	binary_search([np.nan, 1, 2], 1)
-		# 	binary_search([np.nan, 1, 2], np.nan)
-
 		with self.assertRaises(TypeError):
 			binary_search(['a', 'b', 1, 2], 1)
 
@@ -16,6 +13,8 @@ class TestBinSearch(unittest.TestCase):
 
 	def test_boundary(self):
 		self.assertEqual(binary_search([], 1), -1)
+		self.assertEqual(binary_search([2], 2), 0)
+		self.assertEqual(binary_search([3], 2), -1)
 		self.assertEqual(binary_search([1,2,3,4], 1), 0)
 		self.assertEqual(binary_search([1,2,3,4], 4), 3)
 
@@ -23,6 +22,12 @@ class TestBinSearch(unittest.TestCase):
 		self.assertEqual(binary_search(range(10), 11), -1)
 		self.assertEqual(binary_search(range(10), -1), -1)
 		self.assertEqual(binary_search([2,5,7,9], 4), -1)
+
+	def test_range_parameter(self):
+		self.assertEqual(binary_search([3,2,6,4,3,5,6,7,4,3], 3, 4, 7), 4)
+		self.assertEqual(binary_search([3,2,6,4,3,5,6,7,4,3], 5, 4, 7), 5)
+		self.assertEqual(binary_search([3,2,6,4,3,5,6,7,4,3], 4, 4, 7), -1)
+
 
 if __name__ == '__main__':
 	unittest.main()
