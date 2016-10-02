9bc2efebb89c0ccc9619bde73571abf6047d7da9
Peilin Duan <duan@g.harvard.edu>
2016-10-01 21:06:44 -0400

    modify some tests

--- test.py
+++ test.py
@@ -4,12 +4,17 @@ import numpy as np
 
 
 class TestBinSearch(unittest.TestCase):
+	"""
+	The following test cases assume that:
+	* The input list is sorted (thus its elements are comparable)
+	"""
+
+	def test_inf(self):
+		self.assertEqual(binary_search([1, np.inf], 1), 0)
+		self.assertEqual(binary_search([1, 2, np.inf], np.inf), 2)
 
 	def test_nan(self):
-		with self.assertRaises(TypeError):
-			binary_search(['a', 'b', 1, 2], 1)
-
-		self.assertEqual(binary_search(['a', 'b', 'c'], 'b'), 1)
+		self.assertEqual(binary_search(['a', 'b', 'c','d', 'e'], 'b'), 1)
 
 	def test_boundary(self):
 		self.assertEqual(binary_search([], 1), -1)
@@ -21,12 +26,12 @@ class TestBinSearch(unittest.TestCase):
 	def test_not_found(self):
 		self.assertEqual(binary_search(range(10), 11), -1)
 		self.assertEqual(binary_search(range(10), -1), -1)
-		self.assertEqual(binary_search([2,5,7,9], 4), -1)
+		self.assertEqual(binary_search(range(10), 4.5), -1)
 
 	def test_range_parameter(self):
-		self.assertEqual(binary_search([3,2,6,4,3,5,6,7,4,3], 3, 4, 7), 4)
-		self.assertEqual(binary_search([3,2,6,4,3,5,6,7,4,3], 5, 4, 7), 5)
-		self.assertEqual(binary_search([3,2,6,4,3,5,6,7,4,3], 4, 4, 7), -1)
+		self.assertEqual(binary_search([3,3,5,6,7,8], 3, 1), 1)
+		self.assertEqual(binary_search(range(10), 5, 0, 3), -1)
+		self.assertEqual(binary_search(range(10), 4, 2, 1), -1)
 
 
 if __name__ == '__main__':
